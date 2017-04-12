1bc2adc6a32ca9ffe862d70225a3f38d2b571081
JoeBeton <joe_beton@hotmail.co.uk>
2017-04-12 16:53:59 +0100

    Alex is silly

---
+++ mt_tools.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+#This is the library which contains all the functions that we will be writing 
+#to track and box microtubules as well as anything else that we'll be doing.
+
+
+class OpenFile():
+	
+	def __init__(self):
+		#something
+
+class FindMicrotubules():
+
+
+
+class BoxMicrotubules():
+
+
