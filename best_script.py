fffcb0fe0c51657b1b19991fe049e9d118eb1497
JoeBeton <joe_beton@hotmail.co.uk>
2017-04-19 14:19:03 +0100

    added classes and read mrc header

--- best_script.py
+++ best_script.py
@@ -1,1 +1,20 @@
-this is the best script in the world
+#!/usr/bin/env python
+from __future__ import print_function
+
+#This is the script for testing the functions that we've written in mt_tools.py
+#Should probably do test driven design etc but man alive I'm alredy so out of my depth one thing at a time ffs
+
+from mt_tools import OpenFile
+import os
+
+directory = os.listdir('.')
+it_count = 0
+
+for filename in directory:
+	it_count +=1
+	if filename[-4:] == '.mrc':
+		image = OpenFile(filename, it_count)
+		header = image.readMrcHeader(filename)
+
+	else:
+		continue
--- mt_tools.py
+++ mt_tools.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 #This is the library which contains all the functions that we will be writing 
 #to track and box microtubules as well as anything else that we'll be doing.
 
+import numpy as np
+import struct
 
-class OpenFile():
+
+class OpenFile(object):
+	'''
+	A class for opening .mrc image files of motionCorr corrected EM images
 	
-	def __init__(self):
-		#something
+	Not at all sure that this needs to be a class rather than a list of functions but oh well
+	
+	The structure of the mrc header information can be found at: http://www.ccpem.ac.uk/mrc_format/mrc2000.php
+	'''
+	
+	def __init__(self, filename, it_count):
+		
+		self.filename = filename
+		self.it_count = it_count
 
-class FindMicrotubules():
+		
+
+		#some numpy initialisation which is in the Tempy code, no idea what we need to do though
+
+	def readMrcHeader(self, filename):
+		#reads the mrc header lol
+		f = open(filename, 'rb')	#opens the file as read only and reads it as a binary file
+		fm_string = '<'+(10*'l')+(6*'f')+(3*'l')+(3*'f')+(27*'l')+(3*'f')+(4*'c')+'lfl'
+		header = list(struct.unpack(fm_string, f.read(224)))
+		
+		#making a readable annotated version 
+		number = np.arange(60)
+		header_stack = np.column_stack([number[1:],header])
+		print( header_stack)
+
+
+#class FindMicrotubules():
 
 
 
-class BoxMicrotubules():
+#class BoxMicrotubules():
 
 
